0c0de8e939a586c735dcba640f0bd1b56571aa42
Takudzwa Mutamburigwa <tmutamburigwa@byupathway.edu>
2025-04-09 11:50:35 +0200

    database fix

--- config/db.js
+++ config/db.js
@@ -16,7 +16,7 @@ async function connectDB() {
         const opts = {
             bufferCommands: false,
         };
-        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/quiclccart`, opts). then( mongoose => {
+        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/handcraftedhaven`, opts). then( mongoose => {
             return mongoose;
         });
     }
