4eaf88601fa06a91957dadaea847e30fb4c4b176
Takudzwa Mutamburigwa <tmutamburigwa@byupathway.edu>
2025-04-12 15:58:57 +0200

    Roll back

--- config/inngest.js
+++ config/inngest.js
@@ -1,60 +1,56 @@
-import { Inngest } from "inngest";  
+import { Inngest } from "inngest";
 import connectDB from "./db";
-import User from "@/models/User";
+import  User  from "@/models/User"; // Adjust the import path as necessary
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "handcrafted-haven" });
 
-//Inngest function to save user data to a database
-export const syncUserCreate = inngest.createFunction(
+// Inngest function to save user data to MongoDB
+export const syncUserCreation = inngest.createFunction(
   {
-    id: "sync-user-from-clerk"
+    id: "sync-user-creation-from-clerk", // updated ID
   },
-  { event: "clerk/user.created" },
-  async({event}) => {
-    const { id, first_name, last_name, email_addresses, image_url } = event.data
+  { event: "clerk/user.created" }, 
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } = event.data;
     const userData = {
       _id: id,
       email: email_addresses[0].email_address,
-      // biome-ignore lint/style/useTemplate: <explanation>
-      name: first_name + " " + last_name,
-      imageUrl: image_url
-    }
-    await connectDB()
-    await User.create(userData)
+      name: `${first_name} ${last_name}`,
+      imageUrl: image_url,
+    };
+    await connectDB();
+    await User.create(userData);
   }
-)
+);
 
-//Inngest function to update user data in a database
+// Inngest function to update user data to MongoDB
 export const syncUserUpdate = inngest.createFunction(
   {
-    id: "sync-user-update-from-clerk"
+    id: "update-user-from-clerk", // updated ID
   },
   { event: "clerk/user.updated" },
-  async({event}) => {
-    const { id, first_name, last_name, email_addresses, image_url } = event.data
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } = event.data;
     const userData = {
-      _id: id,
+      name: `${first_name} ${last_name}`,
       email: email_addresses[0].email_address,
-      // biome-ignore lint/style/useTemplate: <explanation>
-      name: first_name + " " + last_name,
-      imageUrl: image_url
-    }
-    await connectDB()
+      imageUrl: image_url,
+    };
+    await connectDB();
     await User.findByIdAndUpdate(id,userData)
   }
-)
+);
 
-//Inngest function to delete user from a database
-export const syncUserDelete = inngest.createFunction(
+// Inngest function to delete user data from MongoDB
+export const syncUserDeletion = inngest.createFunction(
   {
-    id: "sync-user-deletion-from-clerk"
+    id: "delete-user-with-clerk", // this one was already unique
   },
   { event: "clerk/user.deleted" },
-  async({event}) => {
-    const { id } = event.data
-
-    await connectDB()
-    await User.findByIdAndDelete(id)
+  async ({ event }) => {
+    const { id } = event.data;
+    await connectDB();
+    await User.findByIdAndDelete(id);
   }
-)
+);
